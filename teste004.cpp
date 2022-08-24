a41b40e8bbd73f60853bfb9e37d385f0d43ed3f7
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2022-08-23 21:05:55 -0300

    up

--- teste004.cpp
+++ teste004.cpp
@@ -1,82 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct no_{
-  int dado;
-  struct no_* prox;
-}no;
+int const MAXTAM=1000;
 
-typedef struct fila_{
-  no* inicio;
-  no* fim;
-}fila;
+int Frente, Tras;
+int Fila[MAXTAM];
 
-no* criar_no (int a){
-  no* n;
-  n=(no*)malloc(sizeof(no));
-  n->dado=a;
-  return n;
-}
-fila* criar_fila(int a){
-  no* n;
-  fila* l;
-  n=criar_no(a);
-  l=(fila*)malloc(sizeof(fila));
-  l->inicio=n;
-  l->fim=n;
-  l->fim->prox=NULL;
-  return l;
+void Fila_Construtor(){
+    Frente=0;
+    Tras=-1;
 }
 
-void inserir( int a, fila* l){
-  no* n;
-  n=criar_no(a);
-  n->prox=NULL;
-  l->fim->prox=n;
-  l->fim=n;
+bool Fila_Vazia(){
+    if(Frente>Tras){
+        return true;
+    }else{
+        return false;
+    }
+
+    //return Frente>Tras;
 }
 
-void remover(fila* l){
-  if(l->inicio==NULL){
-    printf("fila Vazia!\n");
-  }else{
-    no* prev;
-    //printf("%i == %i\n",(int*)l->inicio->dado,a);
-    prev=l->inicio->prox;
-    *l->inicio=*prev;
-    free(prev);
+bool Fila_Cheia(){
+    if(Tras==MAXTAM-1){
+        return true;
+    }else{
+        return false;
     }
 }
 
-void limpar_fila(fila* l){
-  free(l);
+bool Fila_Enfileirar(int valor){
+    if(Fila_Cheia()){
+        return false;
+    }else{
+        Tras++;
+        Fila[Tras]=valor;
+        return true;
+    }
 }
 
-void imprimir(fila* l){
-  if(l->inicio==NULL){
-    printf("fila Vazia!\n");
-  }else{
-    no* i;
-    for(i=l->inicio;i!=NULL;i=i->prox){
-      printf("%i, ",*(int*)i);
+bool Fila_Desenfileirar(int &valor){
+    if(Fila_Vazia()){
+        return false;
+    }else{
+        valor=Fila[Frente];
+        Frente++;
+        return true;
     }
-    printf("\n");
-  }
 }
-int main () {
-  int x=13,y=9;
-  fila* l=NULL;
-  l=criar_fila(x);
-  imprimir(l);
-  printf("----------------------\n");
-  inserir(y,l);
-  imprimir(l);
-  remover(l);
-   printf("----------------------\n");
-   imprimir(l);
-  limpar_fila(l);
-  system("pause");
-  return 0;
- 
-  
+
+bool Fila_Get(int &valor){
+    if(Fila_Vazia()){
+        return false;
+    }else{
+        valor=Fila[Frente];
+        return true;
+    }
+}
+
+int Fila_Tamanho(){
+    return (Tras - Frente)+1;
+}
+
+
+int main(){
+    int Valor;
+    Fila_Construtor();
+
+    Fila_Enfileirar(5);
+    Fila_Enfileirar(2);
+    Fila_Enfileirar(7);
+
+    printf("\n\nTamanho:%d\n\n",Fila_Tamanho());
+
+
+    while( Fila_Desenfileirar(Valor) ){
+        printf("\nValor:%d\n",Valor);
+    }
+    
+
+    /*
+    if( Fila_Desenfileirar(Valor) ){
+        printf("\n\nValor:%d\n\n",Valor);
+    }else{
+        printf("\n\nFila Vazia\n\n");
+    }*/
+
+    system("pause");
+    return 0;
 }
